96d2469151e0a686e113eba264bae8b0dae98864
Leonardo Merza <ljmerza@gmail.com>
2019-07-30 17:02:56 -0400

    add logs

--- custom_components/ourgroceries/__init__.py
+++ custom_components/ourgroceries/__init__.py
@@ -3,8 +3,14 @@ from datetime import timedelta
 import logging
 
 from aiohttp import web
+from ourgroceries import OurGroceries
+import voluptuous as vol
 
 from homeassistant.components import http
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import (CONF_USERNAME, CONF_PASSWORD)
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.discovery import async_load_platform
 from homeassistant.helpers.entity_component import EntityComponent
 
 
@@ -12,14 +18,31 @@ _LOGGER = logging.getLogger(__name__)
 SCAN_INTERVAL = timedelta(seconds=300)
 DOMAIN = 'ourgroceries'
 
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_USERNAME): cv.string,
+        vol.Required(CONF_PASSWORD): cv.string
+    })
+}, extra=vol.ALLOW_EXTRA)
+
 
 async def async_setup(hass, config):
     """Add API for interacting with Our Groceries."""
-    component = hass.data[DOMAIN] = EntityComponent(
-        _LOGGER, DOMAIN, hass, SCAN_INTERVAL, DOMAIN)
+    og_config = config[DOMAIN]
 
-    hass.http.register_view(OurGroceriesView(component))
-    await component.async_setup(config)
+    _LOGGER.debug('creating og instance')
+    hass.data[DOMAIN] = og = OurGroceries(
+        username=og_config[CONF_USERNAME],
+        password=og_config[CONF_PASSWORD])
+
+    _LOGGER.debug('logging into og')
+    await og.login()
+
+    _LOGGER.debug('setting up sensor')
+    hass.async_create_task(async_load_platform(hass, 'sensor', DOMAIN, {}, config))
+
+    hass.http.register_view(OurGroceriesView(og))
+    _LOGGER.debug('registered view')
     return True
 
 
@@ -29,17 +52,18 @@ class OurGroceriesView(http.HomeAssistantView):
     url = '/api/ourgroceries/{entity_id}'
     name = 'api:ourgroceries:ourgroceries'
 
-    def __init__(self, component):
+    def __init__(self, og):
         """Initialize ourgroceries view."""
-        self.component = component
+        self._og = og
 
     async def get(self, request, entity_id):
         """Return ourgroceries list details."""
-        entity = self.component.get_entity(entity_id)
+        _LOGGER.debug('web get')
+
         list_id = request.query.get('list_id')
-
-        if None in (list_id, entity):
+        if list_id is None:
             return web.Response(status=400)
-
-        api_data = await entity.async_grocery_list(request.app['hass'], list_id)
+        _LOGGER.debug('web get list_id {}'.format(list_id))
+        
+        api_data = await self._og.get_list_items(list_id)
         return self.json(api_data)
--- custom_components/ourgroceries/sensor.py
+++ custom_components/ourgroceries/sensor.py
@@ -1,13 +1,7 @@
 """Support for Our Groceries."""
 import logging
 
-from aiohttp import web
-from ourgroceries import OurGroceries
-import voluptuous as vol
-
-from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import (CONF_USERNAME, CONF_PASSWORD)
-import homeassistant.helpers.config_validation as cv
+from . import DOMAIN
 from homeassistant.helpers.entity import Entity
 
 
@@ -16,20 +10,11 @@ _LOGGER = logging.getLogger(__name__)
 ATTR_RECIPES = 'recipes'
 ATTR_SHOPPING_LISTS = 'shopping_lists'
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_USERNAME): cv.string,
-    vol.Required(CONF_PASSWORD): cv.string,
-})
-
 
 async def async_setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the OurGroceries sensor platform."""
-
-    og = OurGroceries(
-        username=config[CONF_USERNAME],
-        password=config[CONF_PASSWORD])
-    await og.login()
-
+    _LOGGER.debug('add_entities')
+    og = hass.data[DOMAIN]
     add_entities([OurGroceriesSensor(og)], True)
 
 
@@ -68,11 +53,6 @@ class OurGroceriesSensor(Entity):
 
     async def async_update(self):
         """Update data from API."""
+        _LOGGER.debug('updating og state')
         self._lists = await self._og.get_my_lists()
-
-    async def async_grocery_list(self, hass, list_id, command=None, item_id=None, item_body=None):
-        """Do something with a list."""
-
-        if command == 'get':
-            return await self._og.get_list_items(list_id)
 
